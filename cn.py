ee3f2709c81da509232a045424b7d3b950ee7a05
Ploeny Parnichthanakom <ploeny@Macintosh.local>
2024-07-23 14:34:17 +0200

    updated apply_CN

--- cn.py
+++ cn.py
@@ -48,26 +48,6 @@ def print_cn_tab(n_list:list[int],e_list:list[tuple]) -> None :
             print(tab[i][j]," ",end="")
         print("\n")
 
-def print_cn_tab_modif(n_list:list[int],e_list:list[tuple]) -> None :
-    tab : list[list[int]] = cn_all(n_list,e_list)
-    
-    print("    ",end="")
-    for i in range(len(n_list)) :
-        print(i," ",end="")
-    print("\n")
-    print("    ",end="")
-    for i in range(len(n_list)+2) :
-        print("- ",end="")
-    print("\n")
-
-    for i in range(len(n_list)) :
-        print(i,"| ",end="")
-        for j in range(i) :
-            print("   ",end="")
-        for j in range(i,len(tab[i])) :
-            print(tab[i][j]," ",end="")
-        print("\n")
-
 def CN(n:int,x:int) -> None :
     n_list : list[int] = g_func.nodes(n)
     e_list : list[tuple] = g_func.edges(n_list)
@@ -92,18 +72,21 @@ def CN(n:int,x:int) -> None :
     print("-- fin --")
 
 def apply_CN(n_list:list[int],e_list:list[tuple]) -> None :
-    print("-- CN scores --")
-    print_cn_tab_modif(n_list,e_list)
-
     print("-- original graph --")
     g_func.graph(n_list,e_list)
     
-    cn_list : list[tuple] = g_func.exclu_edges(e_list,best_cn(n_list,e_list))
+    print("-- CN scores --")
+    print_cn_tab(n_list,e_list)
+    
+    cn_list : list[tuple] = best_cn(n_list,e_list)
     print("proposed edges : ", cn_list)
+    cn_list = g_func.exclu_edges(e_list,best_cn(n_list,e_list))
+    print("new edges : ", cn_list)
+    
 
     e_list = e_list + cn_list
     print("-- new graph --")
     g_func.graph(n_list,e_list)
 #test
-print("-- start --")
-CN(5,2)
+#print("-- start --")
+#CN(5,2)
