aaf8273d45d5a3a92c3e2e996a8dc56325732881
Ploeny Parnichthanakom <ploeny@Macintosh.local>
2024-08-12 23:59:37 +0200

    added pL3N (actually N) function

--- formula.py
+++ formula.py
@@ -57,3 +57,21 @@ def JC(a:set,b:set) -> float :
 # DOUBLE CHECK SIM (CONTEXT?)
 def Sim(edges:set[tuple],x:int,y:int,u:int,v:int) -> float :
     return JC(g_func.N(x),g_func.N(v)) + JC(g_func.N(y),g_func.N(u))
+
+def pL3N(edges:set[tuple],x:int,y:int,sm) -> float :
+    U = g_func.N(x).intersection(g_func.NN(y))
+    V = g_func.N(y).intersection(g_func.NN(x))
+
+    uV = 0.0
+    vU = 0.0
+    xv = 0.0
+    yu = 0.0
+    for u in U :
+        for v in V :
+            if u in g_func.N(v) :
+                uV += sm(g_func.N(u)-{x},V)
+                vU += sm(g_func.N(u)-{y},U)
+                xv += sm(g_func.N(x),g_func.N(v)-{y})
+                yu += sm(g_func.N(y),g_func.N(u)-{x})
+    
+    return sm(g_func.N(x),U) * sm(g_func.N(y),V) * uV * vU * xv * yu
