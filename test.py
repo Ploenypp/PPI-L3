82b1df384b9bd98ef12a22cc9b257fbbe0c1a892
Ploeny Parnichthanakom <ploeny@Macintosh.local>
2024-08-16 14:35:53 +0200

    testing

--- test.py
+++ test.py
@@ -4,14 +4,28 @@ import calc_n
 import formula
 
 yeast = d_func.file_set("Yeast.txt")
-print(yeast)
 ref = d_func.reference(yeast)
-print(ref)
 e = d_func.convert(ref,yeast)
 n = g_func.get_nodes(e)
-print(len(n))
 
-SR = calc_n.cand(n,e,True,True,formula.pL3Np,formula.SR)
+print(len(n))
+num_n = (int)(len(n)/2)
+print(num_n)
+
+new_n = []
+for i in range(num_n) :
+    new_n.append(i)
+print(len(new_n))
+
+new_e = set()
+for (a,b) in e :
+    if a <= num_n and b <= num_n :
+        new_e.add(tuple((a,b)))
+print(len(new_e))
+
+#SR = calc_n.cand(new_n,new_e,True,True,formula.pL3Np,formula.SR)
 JC = calc_n.cand(n,e,True,True,formula.pL3Np,formula.JC)
-print("len(SR) = ",len(SR))
+#print("len(SR) = ",len(SR))
+#print(SR)
 print("len(JC) = ",len(JC))
+print(JC)
