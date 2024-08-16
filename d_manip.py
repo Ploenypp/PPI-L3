4ff896765808c2adfcd78c068fde054cae115c8a
Ploeny Parnichthanakom <ploeny@Macintosh.local>
2024-08-16 20:39:55 +0200

    updated

--- d_manip.py
+++ d_manip.py
@@ -3,11 +3,20 @@ import g_func
 import d_func
 import formula
 
-e_file = d_func.file_set("Yeast.txt")
+e_file = set()
+
+# OPEN DATASET (CSVFILE)
+with open("Yeast.csv","r") as csvfile :
+    csvtxt = csv.reader(csvfile)
+    for (a,b) in csvtxt :
+        if a!= "source" and b!= "target" :
+            e_file.add(tuple((a,b)))
+
 ref = d_func.reference(e_file)
 e = d_func.convert(ref,e_file)
 n = g_func.get_nodes(e)
 
+# TAKE SUBSET OF DATASET
 num_n = (int)(len(n)/10)
 new_n = []
 i = 1
@@ -20,6 +29,7 @@ for (a,b) in e :
     if a <= num_n and b <= num_n :
         new_e.add(tuple((a,b)))
 
+# RECORD SCORES FOR ALL EGGES IN SUBSET
 data = [["n1","n2","score"]]
 for i in new_n :
     for j in new_n :
