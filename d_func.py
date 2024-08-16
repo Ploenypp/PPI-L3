060242384d146790a8ff72e7ec97718951e2b65f
Ploeny Parnichthanakom <ploeny@Macintosh.local>
2024-08-16 20:26:07 +0200

    removed test from d_func

--- d_func.py
+++ d_func.py
@@ -74,27 +74,3 @@ def cand(edges) :
     record(max_val,res)
     return res
 
-
-# test
-"""e_file = file_set("Yeast.txt")
-ref = reference(e_file)
-e = convert(ref,e_file)
-n = g_func.get_nodes(e)
-print(n)
-
-num_n = (int)(len(n)/10)
-print(num_n)
-new_n = []
-i = 1
-while i < num_n + 1 :
-    new_n.append(i)
-    i += 1
-print(len(new_n))
-
-new_e = set()
-for (a,b) in e :
-    if a <= num_n and b <= num_n :
-        new_e.add(tuple((a,b)))
-print(len(new_e))
-
-candidates = cand(new_e)"""
