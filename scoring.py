ce945b1febca4254410848deedcb528d09c86fa0
Ploenypp <everhonorlight@gmail.com>
2025-05-08 16:47:53 +0200

    initial testing with synthetic PPI dataset

--- scoring.py
+++ scoring.py
@@ -1,10 +1,11 @@
 import math
+from itertools import product
 from collections import Counter
 
 import pandas as pd
 
 # calculate scores for every possible pair with give method
-def scores(G,method,metric,ex=True) :
+def scores(G, method, metric, ex=True) :
     nodes = sorted(list(G.nodes))
     res = dict()
 
@@ -15,7 +16,7 @@ def scores(G,method,metric,ex=True) :
             else : res[(nodes[i],nodes[j])] = method(G,nodes[i],nodes[j],metric,ex)
             j += 1
 
-    return res #dict(Counter(res).most_common())
+    return res 
 
 # create dataframe 
 def convert_df(scores) :
@@ -24,3 +25,19 @@ def convert_df(scores) :
     res = {'pairs':pairs, 'scores':values}
     return pd.DataFrame(res)
 
+def score_candidates(G, candidate_edges, method, metric, ex=True) :
+    res = dict() 
+
+    for n1,n2 in candidate_edges :
+        if metric == None : res[(n1,n2)] = method(G,n1,n2)
+        else : res[(n1,n2)] = method(G,n1,n2,metric,ex)
+
+    return res
+
+def top_N_candidates(edges_scores,n) :
+    ranked = dict(Counter(edges_scores).most_common())
+    if n == all : return ranked
+    res = dict()
+    for edge, score in ranked.items() :
+        if len(res) == n : return res
+        res[edge] = score
